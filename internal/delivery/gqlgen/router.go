2fd02cc9855055ed6da13b1e9d64d33d07fe5227
gleamsoda <gleamsoda99@gmail.com>
2023-10-09 04:09:03 +0900

    Add chi middleware

--- internal/delivery/gqlgen/router.go
+++ internal/delivery/gqlgen/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 
 	"playground/internal/delivery/gqlgen/gen"
 	"playground/internal/delivery/gqlgen/resolver"
@@ -14,7 +15,12 @@ func NewRouter(r *resolver.Resolver, tm token.Manager) *chi.Mux {
 	h := handler.NewDefaultServer(gen.NewExecutableSchema(gen.Config{Resolvers: r}))
 
 	router := chi.NewRouter()
-	router.Use(MetadataMiddleware, AuthMiddlewareFunc(tm))
+	router.Use(
+		middleware.Logger,
+		middleware.Recoverer,
+		MetadataMiddleware,
+		AuthMiddlewareFunc(tm),
+	)
 	router.Get("/", playground.Handler("GraphQL playground", "/query"))
 	router.Post("/query", h.ServeHTTP)
 	return router
